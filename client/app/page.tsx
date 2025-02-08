f0fec8932687aa4430995a7db9b17dc08f0a2f36
Shunsuke Fujinawa <shun26497204@gmail.com>
2025-02-08 22:02:26 +0900

    refactor: 不要コメント削除

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,7 @@
 "use client"
 
 import React, { useState, useEffect } from 'react';
-import { Quiz } from '../components/Quiz';
 import { Question } from '../types/QuizType';
-// import { fetchQuizQuestions } from './utils/quizUtils';
-import { QuizSection } from '../components/QuizSection';
 import '../global.css';
 import { Header } from '../components/organisms/Header';
 import { Categories } from '../components/molecules/Categories';
@@ -28,31 +25,13 @@ const App = () => {
   const startTrivia = async () => {
     setLoading(true);
     setGameOver(false);
-
-    // const newQuestions = await fetchQuizQuestions(TOTAL_QUESTIONS);
-
-    // setQuestions(newQuestions);
     setScore(0);
     setUserAnswers([]);
     setNumber(0);
     setLoading(false);
   };
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-    if (!gameOver) {
-      // User's answer
-      const answer = e.currentTarget.value;
-      // Check answer against correct answer
-      // const correct = questions[number].correct_answer === answer;
-      // Add score if answer is correct
-      // if (correct) setScore(prev => prev + 1);
-      // Save answer in the array for user answers
-      setUserAnswers(prev => [...prev, answer]);
-    }
-  };
-
   const nextQuestion = () => {
-    // Move on to the next question if not the last question
     const nextQuestion = number + 1;
 
     if (nextQuestion === TOTAL_QUESTIONS) {
@@ -61,36 +40,11 @@ const App = () => {
       setNumber(nextQuestion);
     }
   };
-  const router = useRouter()
-  const searchParams = useParams()
-  const pathName = usePathname()
 
   return (
-    <AuroraBackground>
+    <AuroraBackground showRadialGradient={true}>
       <Header />
       <Categories />
-      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
-        <button className="text-text-white" onClick={startTrivia}>
-          Start
-        </button>
-      ) : null}
-      {gameOver ? null : <p className="text-text-white">Score: {score}</p>}
-      {loading && <p>Loading Questions...</p>}
-      {/* {!loading && !gameOver && (
-        <Quiz
-          questionNr={number + 1}
-          totalQuestions={TOTAL_QUESTIONS}
-          question={questions[number].question}
-          answers={questions[number].answers}
-          userAnswer={userAnswers ? userAnswers[number] : undefined}
-          callback={checkAnswer}
-        />
-      )} */}
-      {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
-        <button className="next" onClick={nextQuestion}>
-          Next Question
-        </button>
-      ) : null}
     </AuroraBackground>
   );
 };
--- client/components/molecules/Category.tsx
+++ client/components/molecules/Category.tsx
@@ -8,7 +8,6 @@ export const Category = ({ categoryName }: CategoryProps) => {
   return (
     <div className="p-2 sm:w-1/4 w-full hover:scale-105 duration-300">
       <Link href={`/QuizSection?lang=${categoryName}`}>
-        <button className="block w-full">
           <div className="bg-transparent rounded flex p-4 items-center hover:bg-background-main outline-zinc-500 outline-2 outline">
             <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="text-zinc-100 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
               <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
@@ -16,7 +15,6 @@ export const Category = ({ categoryName }: CategoryProps) => {
             </svg>
             <span className="text-white font-bold text-xl">{categoryName}</span>
           </div>
-        </button>
       </Link>
     </div>
   )
